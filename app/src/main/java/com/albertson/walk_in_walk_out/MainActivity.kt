4ad4a5c16a9d88fa15f8bd1271b635ecf7207e7e
SAFEWAY01\KANNA03 <kwaw.annan@albertsons.com>
2022-08-01 11:56:59 -0700

    Base UI components

--- app/src/main/java/com/albertson/walk_in_walk_out/MainActivity.kt
+++ app/src/main/java/com/albertson/walk_in_walk_out/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.albertson.walk_in_walk_out
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Fragment
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.albertson.walk_in_walk_out.databinding.ActivityMainBinding
+import com.albertson.walk_in_walk_out.ui.homeFragment.viewModel.HomeFragment
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +13,29 @@ class MainActivity : AppCompatActivity() {
         var binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.fragment_container_view, HomeFragment::class.java, null)
+                .commit()
+        }
+    }
+
+    private fun navigateToFragment(fragment: androidx.fragment.app.Fragment) {
+        val manager: android.app.FragmentManager? = fragmentManager
+        supportFragmentManager.beginTransaction()
+            .setReorderingAllowed(true)
+            .add(R.id.fragment_container_view, HomeFragment::class.java, null)
+            .commit()
+
+
+    }
+    fun replaceFragment(fragment: androidx.fragment.app.Fragment){
+        val manager: android.app.FragmentManager? = fragmentManager
+        supportFragmentManager.beginTransaction()
+            .setReorderingAllowed(true)
+            .replace(R.id.fragment_container_view, HomeFragment::class.java, null)
+            .commit()
+
     }
 }
